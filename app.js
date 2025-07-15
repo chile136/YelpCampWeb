5ebc45f50d1eea2cc084d0653385216d43245799
chile136 <leducchi1998@gmail.com>
2025-07-15 20:09:23 +0700

    fix: MongoDB Alats

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ const usersRouter = require("./routes/users");
 const campgroundsRouter = require("./routes/campgrounds");
 const reviewsRouter = require("./routes/reviews");
 
-// const dbUrl = process.env.DB_URL;
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL;
+// const dbUrl = "mongodb://localhost:27017/yelp-camp";
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
